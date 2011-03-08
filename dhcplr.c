deed1874db185a5df05b90443e4abac7b15e8081
Miguel Serrano <miguelvps@gmail.com>
2011-03-08 00:09:58 +0000

    Add error checking

--- dhcplr.c
+++ dhcplr.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <time.h>
 
+#include <net/ethernet.h>
 #include <arpa/inet.h>
 #include <netinet/ether.h>
 #include <netinet/in.h>
@@ -14,36 +15,55 @@
 
 int main(int argc, const char *argv[]) {
 
-    int s;
+    int s, r;
     struct sockaddr_in sa;
     struct dhcp_packet release;
-    const char *siaddr, *ciaddr, *chaddr;
+    struct in_addr siaddr, ciaddr;
+    struct ether_addr *chaddr;
 
     if (argc != 4) {
-        printf("Usage: %s SIADDR CIADDR CHADDR\n", argv[0]);
-        return 1;
+        fprintf(stderr, "Usage: %s SIADDR CIADDR CHADDR\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    siaddr = argv[1];
-    ciaddr = argv[2];
-    chaddr = argv[3];
+    r = inet_pton(AF_INET, argv[1], &siaddr);
+    if (!r) {
+        fprintf(stderr, "%s: '%s' is not a valid SIADDR\n", argv[0], argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    r = inet_pton(AF_INET, argv[2], &ciaddr);
+    if (!r) {
+        fprintf(stderr, "%s: '%s' is not a valid CIADDR\n", argv[0], argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    chaddr = ether_aton(argv[3]);
+    if (chaddr == NULL) {
+        fprintf(stderr, "%s: '%s' is not a valid CHADDR\n", argv[0], argv[3]);
+        exit(EXIT_FAILURE);
+    }
+
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (!s) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
     srand(time(NULL));
 
-    memset(&release, 0, sizeof(release));
-
     sa.sin_family = AF_INET;
     sa.sin_port = htons(DHCP_SERVER_PORT);
-    sa.sin_addr.s_addr = inet_addr(siaddr);
+    sa.sin_addr = siaddr;
 
-    s = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&release, 0, sizeof(release));
 
     release.op = BOOTREQUEST;
     release.htype = HTYPE_ETHER;
     release.hlen = 6;
     release.xid = rand();
-    release.ciaddr.s_addr = inet_addr(ciaddr);
-    memcpy(release.chaddr, (void *)ether_aton(chaddr), 6);
+    release.ciaddr = ciaddr;
+    memcpy(release.chaddr, (void *)chaddr, 6);
 
     // start options
     memcpy(release.options, (void *)DHCP_OPTIONS_COOKIE, 4);
@@ -54,15 +74,18 @@ int main(int argc, const char *argv[]) {
 
     release.options[7] = DHO_DHCP_SERVER_IDENTIFIER;
     release.options[8] = 4;
-    memcpy(release.options+9, (void *)&sa.sin_addr.s_addr, 4);
+    memcpy(release.options+9, (void *)&siaddr.s_addr, 4);
 
     release.options[13] = DHO_END;
     // end options
 
-    sendto(s, &release, sizeof(release), 0, (const struct sockaddr *)&sa,
-            sizeof(sa));
+    r = sendto(s, &release, sizeof(release), 0, (const struct sockaddr *)&sa,
+               sizeof(sa));
 
-    printf("%s\n", strerror(errno));
+    if (!r) {
+        perror("sendto");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
